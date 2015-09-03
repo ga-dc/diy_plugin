9fa1aef7728598122ba3f1c28c399be03eef24e8
mattscilipoti <matt.scilipoti@ga.co>
2015-09-03 12:04:34 -0400

    greenify works

--- js/jquery.greenify.js
+++ js/jquery.greenify.js
@@ -11,8 +11,21 @@
   // You can now reference the window, document, and jQuery objects in a local scope
   //   Using the variables you defined above.
 
-  $.fn.greenify = function() {
-    this.css( "color", "BurlyWood" );
-  };
+  $.fn.greenify = function( options ) {
+
+        // This is the easiest way to have default options.
+        var settings = $.extend({
+            // These are the defaults.
+            color: "#bada55",
+            backgroundColor: "white"
+        }, options );
+
+        // Greenify the collection based on the settings variable.
+        return this.css({
+            color: settings.color,
+            backgroundColor: settings.backgroundColor
+        });
+
+    };
 
 }(window, document, window.jQuery)); // The global window, document, and jQuery objects are passed into the anonymous function
